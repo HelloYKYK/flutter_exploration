e37510324c5adaf54b633eddcb4279602e72a26a
zhaohan <zhaohan1@yiche.com>
2019-03-12 13:25:08 +0800

    i dont known

--- lib/widget_basis/InputWidget.dart
+++ lib/widget_basis/InputWidget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InputWidget extends StatelessWidget {
-
   const InputWidget({
     Key key,
     @required this.title,
@@ -21,9 +20,34 @@ class InputWidget extends StatelessWidget {
   }
 
   Widget _buildBody() {
+    TextEditingController _unameController = new TextEditingController();
+    TextEditingController _upasswordController = new TextEditingController();
+
     return Column(
-      
+      children: <Widget>[
+        TextField(
+          autofocus: true,
+          decoration: InputDecoration(
+              labelText: "用户名",
+              hintText: "用户名或邮箱",
+              prefixIcon: Icon(Icons.person)),
+          controller: _unameController,
+        ),
+        TextField(
+          autofocus: true,
+          decoration: InputDecoration(
+              labelText: "密码", hintText: "登录密码", prefixIcon: Icon(Icons.lock)),
+          obscureText: true,
+          controller: _upasswordController,
+        ),
+        RaisedButton(
+          child: Text("登录"),
+          onPressed: () {
+            print(_unameController.text);
+            print(_upasswordController.text);
+          },
+        )
+      ],
     );
   }
-
 }
